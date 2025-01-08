e8d47d648a6e9f253b2990c11b2fd3df09955b36
nhtanvir22 <tanvir.eatl@gmail.com>
2025-01-08 12:26:07 +0600

    Active link added

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,12 +1,17 @@
+"use client";
+
 import React from "react";
 import Link from "next/link";
 import { AiFillBug } from "react-icons/ai";
+import { usePathname } from "next/navigation";
+import classNames from "classnames";
 
 const NavBar = () => {
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
   ];
+  const currentPath = usePathname();
 
   return (
     <nav className="border-b flex h-16 items-center px-6 mb-6 space-x-6">
@@ -17,7 +22,11 @@ const NavBar = () => {
         {links.map((link, index) => (
           <li key={index}>
             <Link
-              className="text-zinc-500 hover:text-zinc-800 transition-colors"
+              className={classNames({
+                "text-zinc-900 pointer-events-none": currentPath === link.href,
+                "text-zinc-500 hover:text-zinc-800 transition-colors":
+                  currentPath !== link.href,
+              })}
               href={link.href}
             >
               {link.label}
---
+++ app/issues/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const IssuesPage = () => {
+  return (
+    <div>
+      <h2>Issues Page</h2>
+    </div>
+  );
+};
+
+export default IssuesPage;
