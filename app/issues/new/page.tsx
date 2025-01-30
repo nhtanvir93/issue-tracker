85c788d4ac4227445225bf1b5a8866d8e79d12ec
nhtanvir22 <tanvir.eatl@gmail.com>
2025-01-30 13:22:56 +0600

    server validation error for create issue form added

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,6 +1,6 @@
 "use client";
-import React from "react";
-import { TextField, Heading } from "@radix-ui/themes";
+import React, { useState } from "react";
+import { TextField, Heading, Callout } from "@radix-ui/themes";
 import { Button } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import { useForm, Controller } from "react-hook-form";
@@ -16,32 +16,47 @@ interface IssueForm {
 const NewIssuePage = () => {
   const router = useRouter();
   const { register, control, handleSubmit } = useForm<IssueForm>();
+  const [error, setError] = useState("");
 
   return (
-    <form
-      className="max-w-xl space-y-4"
-      onSubmit={handleSubmit(async (data) => {
-        await axios.post("/api/issues", data);
-        router.push("/issues");
-      })}
-    >
-      <Heading size="7">Create New Issue</Heading>
-      <TextField.Root placeholder="Title" {...register("title")}>
-        <TextField.Slot />
-      </TextField.Root>
-      <Controller
-        name="description"
-        control={control}
-        render={({ field }) => (
-          <SimpleMDE placeholder="Description" {...field} />
-        )}
-      />
-      <div className="flex justify-end">
-        <Button type="submit" variant="solid">
-          Submit
-        </Button>
-      </div>
-    </form>
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" role="alert" className="mb-6">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className="space-y-4"
+        onSubmit={handleSubmit(async (data) => {
+          setError("");
+
+          try {
+            await axios.post("/api/issues", data);
+            router.push("/issues");
+          } catch (error) {
+            setError("An unexpected error occurred");
+          }
+        })}
+      >
+        <Heading size="7">Create New Issue</Heading>
+        <TextField.Root placeholder="Title" {...register("title")}>
+          <TextField.Slot />
+        </TextField.Root>
+        <Controller
+          name="description"
+          control={control}
+          defaultValue=""
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
+        />
+        <div className="flex justify-end">
+          <Button type="submit" variant="solid">
+            Submit
+          </Button>
+        </div>
+      </form>
+    </div>
   );
 };
 
