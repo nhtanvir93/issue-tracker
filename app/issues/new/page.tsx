35ac069e1e1aab39bb80f486b9a94175c63fb494
nhtanvir22 <tanvir.eatl@gmail.com>
2025-01-30 12:12:21 +0600

    new issue submission with react-hook-form and axios done

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -3,20 +3,45 @@ import React from "react";
 import { TextField, Heading } from "@radix-ui/themes";
 import { Button } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 import "easymde/dist/easymde.min.css";
 
+interface IssueForm {
+  title: string;
+  description: string;
+}
+
 const NewIssuePage = () => {
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+
   return (
-    <div className="max-w-xl space-y-4">
+    <form
+      className="max-w-xl space-y-4"
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post("/api/issues", data);
+        router.push("/issues");
+      })}
+    >
       <Heading size="7">Create New Issue</Heading>
-      <TextField.Root placeholder="Title">
+      <TextField.Root placeholder="Title" {...register("title")}>
         <TextField.Slot />
       </TextField.Root>
-      <SimpleMDE placeholder="Description" />
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE placeholder="Description" {...field} />
+        )}
+      />
       <div className="flex justify-end">
-        <Button variant="solid">Submit</Button>
+        <Button type="submit" variant="solid">
+          Submit
+        </Button>
       </div>
-    </div>
+    </form>
   );
 };
 
