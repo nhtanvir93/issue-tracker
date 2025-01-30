ed1556a3c0563bd81902e28dd9ce74057582834f
nhtanvir22 <tanvir.eatl@gmail.com>
2025-01-30 11:24:15 +0600

    Radix theme modified

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import NavBar from "./NavBar";
 import "@radix-ui/themes/styles.css";
 import { Theme } from "@radix-ui/themes";
+import { ThemePanel } from "@radix-ui/themes";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +31,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Theme>
+        <Theme accentColor="violet" radius="small">
           <NavBar />
           <main className="p-5">{children}</main>
         </Theme>
